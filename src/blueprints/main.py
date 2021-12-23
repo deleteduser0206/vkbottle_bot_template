a454929ad0ffea602e65940492b0715f7f2d4d1f
deleteduser0206 <deleteduser0206@gmail.com>
2021-12-23 18:20:49 +0200

    Improved code readability and updated docstrings.

--- src/blueprints/main.py
+++ src/blueprints/main.py
@@ -17,7 +17,7 @@ bp.labeler.vbml_ignore_case = True
 @bp.on.message(text=["/начать", "/start"])
 async def start_handler(_):
     """Обработчик, который приветствует при команде /start|/начать или payload: {"cmd": "start"}"""
-    return "Привет!\n" "Это пример бота.\n" "Узнать мои команды: /help"
+    return "Привет!\n" "Это пример бота.\nУзнать мои команды: /help"
 
 
 @bp.on.message(text=["/register", "/register <!>"])
@@ -27,7 +27,7 @@ async def register_handler(m: Message):
     if await user.register(ctx_api=m.ctx_api):
         return "Вы успешно зарегистрировались."
 
-    return f"Вы уже зарегистрированы, ваш профиль:\n\n" f"{await user.get_prepared_user_info()}"
+    return f"Вы уже зарегистрированы, ваш профиль:\n\n{await user.get_prepared_user_info()}"
 
 
 @bp.on.message(
@@ -38,23 +38,23 @@ async def register_handler(m: Message):
     ]
 )
 async def delete_account_handler(m: Message):
-    """Обработчик с удалением пользователя"""
+    """Обработчик для удаления пользователя по его команде"""
     if await UserRepository(uid=m.from_id).unregister():
         return "Вы успешно удалили аккаунт."
 
-    return "Вы не зарегистрированы. \n" "Введите /register"
+    return "Вы не зарегистрированы. \nВведите /register"
 
 
 @bp.on.message(text=["/profile", "/profile <id_:int>", "/profile [id<id_:int>|<!>]"])
 async def profile_handler(m: Message, id_: Optional[int] = None):
-    """Обработчик отправки информации о пользователе"""
+    """Обработчик получения информации о пользователе"""
     if await IsAdminRule().check(event=m) and isinstance(id_, int):
         user = await UserRepository(uid=id_).get_full_user_info()
         if not user:
             return "Пользователя нет в базе данных."
 
         info = UserRepository.prepared_info(user=user)
-        return f"Профиль пользователя [id{m.from_id}|{user.first_name}]:\n" f"{info}"
+        return f"Профиль пользователя [id{m.from_id}|{user.first_name}]:\n{info}"
 
     user = await UserRepository(uid=m.from_id).get_prepared_user_info()
     return user
@@ -65,15 +65,15 @@ async def current_time_handler(m: Message):
     """
     Обработчик с отправкой текущего времени пользователю,
     с использованием времени от события,
-    которое приходит вместе с сообщением от пользователя
+    которое приходит вместе с сообщением
     """
-    datetime_template = "Текущее время: %H:%M:%S\n" "Дата: %d.%m.%Y"
+    datetime_template = "Текущее время: %H:%M:%S\nДата: %d.%m.%Y"
     return datetime.fromtimestamp(m.date).strftime(datetime_template)
 
 
 @bp.on.message(text=["/command<!>", "/help<!>"])
 async def help_list_handler(_):
-    """Список команд"""
+    """Выводит список команд пользователю"""
     text = (
         "Мой список команд:\n"
         "/register - Зарегистрировать аккаунт\n"
@@ -88,4 +88,4 @@ async def help_list_handler(_):
 async def chat_invite_handler(m: Message):
     """Обработчик с информацией о том, что нашего бота пригласили"""
     if m.action.member_id == -config.bot.info.group_id:
-        return "Спасибо что пригласили меня!\n" "Введите /help для получения списка команд."
+        return "Спасибо что пригласили меня!\nВведите /help для получения списка команд."
