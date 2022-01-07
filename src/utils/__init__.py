6ee7bdb75032a59d3d10dd415a5a77988fe77ddb
deleteduser0206 <deleteduser0206@gmail.com>
2022-01-07 20:58:42 +0200

    Оптимизация кода 2

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Модуль с утилитами,
-которые по каким то причинам не могут быть в другом месте.
+Модуль с утилитами, которые по каким-то причинам не могут быть в другом месте.
 """
-from src.utils.loop import loop, loop_wrapper
+from uvloop import EventLoopPolicy
+from vkbottle.tools.dev.loop_wrapper import LoopWrapper
+
+loop = EventLoopPolicy().get_event_loop()
+loop_wrapper = LoopWrapper()
+
 
 __all__ = (
     "loop",
